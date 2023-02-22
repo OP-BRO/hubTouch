54ac4c2628f2d520c81ada3b2f74fe577054db6a
OP-BRO <91185325+OP-BRO@users.noreply.github.com>
2023-02-22 17:23:57 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-# Get a cpu or gpu device for training.
+# Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
